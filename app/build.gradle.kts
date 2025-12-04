e9cf5567b071d1c55ce1b7b434d0901628168ad1
Aswin C <aswinanand9020@gmail.com>
2025-12-05 00:50:43 +0530

    chore: Bump version to 1.12.0 and fix game assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.boxboxbox.f1tracker.live"
         minSdk = 26
         targetSdk = 34
-        versionCode = 34
-        versionName = "1.11.0"
+        versionCode = 35
+        versionName = "1.12.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
